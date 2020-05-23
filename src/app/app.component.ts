efcb68955e371e0d8eb8a8bf9c8741b910fd87ee
Nick <nick@networkbasedsolutions.com>
2020-05-22 18:28:33 -0700

    Added basic file input handling.
    * Removed template code.
    * Added basic file input handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-slacker-to-spotify';
+  title = 'Slacker to Spotify';
+  contents = '';
+
+  handleFileInput(inputEvent: Event) {
+    const files: FileList = (inputEvent.target as HTMLInputElement).files;
+    if (files && files.item(0)) {
+      const fileReader = new FileReader();
+
+      fileReader.onload = (readerEvent) => {
+        if (typeof readerEvent.target.result === 'string') {
+          this.contents = atob(readerEvent.target.result.split(',')[1]);
+        }
+      };
+
+      fileReader.readAsDataURL(files.item(0));
+    }
+  }
 }
