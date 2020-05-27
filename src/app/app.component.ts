3cd3b0731927274fd23379021d12fdf627c839fd
Nick <nick@networkbasedsolutions.com>
2020-05-27 15:15:38 -0700

    Added Spotify authentication.
    * Added logic to display Spotify's access token or a "grant" link.
    * Parsed and displayed the access token if it exists.
    * Hack: Added a default empty route in order to parse the access token.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Song} from './song';
 import {Playlist} from './playlist';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Slacker to Spotify';
   playlistName = '';
   songs: Song[] = [];
+  spotifyAuthUrl: string;
+  spotifyAccessToken: string;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.spotifyAuthUrl = this.generateSpotifyAuthUrl();
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe(hash => {
+      if (hash) {
+        this.spotifyAccessToken = (new URLSearchParams(hash)).get('access_token');
+      }
+    });
+  }
+
+  generateSpotifyAuthUrl(): string {
+    const clientId = 'ee26bb61755e44c5b7e7a0a29c0f7ed5';
+    const scope = 'user-read-private user-read-email';
+    const redirectUrl = 'http://localhost:4200/';
+
+    let url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(clientId);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirectUrl);
+
+    return url;
+  }
 
   handleFileInput(inputEvent: Event) {
     const files: FileList = (inputEvent.target as HTMLInputElement).files;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
