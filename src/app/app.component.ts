e74d9f2eeda7af653423968a37496b055c72f431
Nick <nick@networkbasedsolutions.com>
2020-05-28 19:20:40 -0700

    Made the app wait for the Spotify song data to load.
    * Moved the Spotify "search tracks" logic into a method that returns a promise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,24 +70,38 @@ export class AppComponent implements OnInit {
           this.playlistName = playlist.getPlaylistName();
           this.songs = playlist.getSongs();
 
-          for (const song of this.songs) {
-            const query = `"${song.title}" artist:"${song.artist}"`;
-            this.spotify.searchTracks(query, {limit: 1}, (err, data) => {
-              if (data.tracks.total > 0) {
-                song.uri = data.tracks.items[0].uri;
-                song.previewUrl = data.tracks.items[0].preview_url;
-                song.externalUrl = data.tracks.items[0].external_urls.spotify;
-              }
-
-              if (song.previewUrl === null) {
-                console.log(data);
-              }
-            });
-          }
+          this.loadSpotifyData().then(() => {
+            console.log(this.songs.filter(song => song.uri != null).length);
+          });
         }
       };
 
       fileReader.readAsDataURL(files.item(0));
     }
+  }
+
+  private loadSpotifyData(): Promise<any> {
+    const promises = [];
+
+    for (const song of this.songs) {
+      const query = `"${song.title}" artist:"${song.artist}"`;
+      const searchResults = this.spotify.searchTracks(query, {limit: 1});
+
+      searchResults.then((data) => {
+        if (data.tracks.total > 0) {
+          song.uri = data.tracks.items[0].uri;
+          song.previewUrl = data.tracks.items[0].preview_url;
+          song.externalUrl = data.tracks.items[0].external_urls.spotify;
+        }
+
+        if (song.previewUrl === null) {
+          console.log(data);
+        }
+      });
+
+      promises.push(searchResults);
+    }
+
+    return Promise.all(promises);
   }
 }
