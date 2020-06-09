6e9eb6082a3217a21ace12a2e2e751ea53b13794
Nick <nick@networkbasedsolutions.com>
2020-06-08 19:24:06 -0700

    Added a visual filter for the displayed songs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent implements OnInit {
   title = 'Slacker to Spotify';
   playlistName = '';
   songs: Song[] = [];
-  songsUnmatched: Song[] = [];
-  songsLoaded = {count: 0};
+  songsKnown: Song[] = [];
+  songsUnknown: Song[] = [];
+  songsLoaded = {count: 0, finished: false};
+  songsDisplayed: Song[] = [];
 
   constructor(public spotifyService: SpotifyService) {
   }
@@ -24,8 +26,10 @@ export class AppComponent implements OnInit {
   handleFileInput(inputEvent: Event) {
     this.playlistName = '';
     this.songs = [];
-    this.songsUnmatched = [];
-    this.songsLoaded = {count: 0};
+    this.songsKnown = [];
+    this.songsUnknown = [];
+    this.songsLoaded = {count: 0, finished: false};
+    this.songsDisplayed = [];
 
     const files: FileList = (inputEvent.target as HTMLInputElement).files;
     if (files && files.item(0)) {
@@ -38,11 +42,15 @@ export class AppComponent implements OnInit {
 
           this.playlistName = playlist.getPlaylistName();
           this.songs = playlist.getSongs();
+          this.songsDisplayed = this.songs;
 
           console.log(`handleFileInput: ${this.songs.length} songs parsed`);
 
           this.spotifyService.loadSongData(this.songs, this.songsLoaded).then(() => {
-            this.songsUnmatched = this.songs.filter(song => song.uri === undefined);
+            this.songsLoaded.finished = true;
+
+            this.songsKnown = this.songs.filter(song => song.uri);
+            this.songsUnknown = this.songs.filter(song => song.uri === undefined);
 
             this.spotifyService.loadUserId().then(userId => {
               this.spotifyService.createPlaylist(userId, this.playlistName, this.songs).then(playlistId => {
@@ -56,6 +64,23 @@ export class AppComponent implements OnInit {
       };
 
       fileReader.readAsDataURL(files.item(0));
+    }
+  }
+
+  handleSongFilterChange(inputEvent: Event) {
+    const filter = (inputEvent.target as HTMLInputElement).value;
+    switch (filter) {
+      case 'all':
+        this.songsDisplayed = this.songs;
+        break;
+
+      case 'known':
+        this.songsDisplayed = this.songsKnown;
+        break;
+
+      case 'unknown':
+        this.songsDisplayed = this.songsUnknown;
+        break;
     }
   }
 }
