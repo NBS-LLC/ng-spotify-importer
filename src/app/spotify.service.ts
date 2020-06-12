7d1d8a0cf9a911af322f824bfec145a7ef63eb3f
Nick <nick@networkbasedsolutions.com>
2020-06-11 19:14:36 -0700

    Revert "Moved Spotify's client id."
    This reverts commit 4063e90d

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -6,21 +6,19 @@ import {Song} from './song';
   providedIn: 'root'
 })
 export class SpotifyService {
-  private static clientId = 'ee26bb61755e44c5b7e7a0a29c0f7ed5'; // TODO: Move this elsewhere.
-
   private spotifyWebApi = new SpotifyWebApi();
   private authenticated = false;
 
   constructor() {
   }
 
-  getAuthUrl() {
+  getAuthUrl(clientId: string) {
     const scope = 'user-read-private playlist-modify-public';
     const redirectUrl = 'http://localhost:4200/';
 
     let url = 'https://accounts.spotify.com/authorize';
     url += '?response_type=token';
-    url += '&client_id=' + encodeURIComponent(SpotifyService.clientId);
+    url += '&client_id=' + encodeURIComponent(clientId);
     url += '&scope=' + encodeURIComponent(scope);
     url += '&redirect_uri=' + encodeURIComponent(redirectUrl);
 
