16f05f871733080d274b0dce07d165f3c04b7165
Nick <nick@networkbasedsolutions.com>
2020-06-16 19:59:58 -0700

    Preparing production configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  spotify: {
+    clientId: '710c3609ed2b4605b3a17464001b1398',
+    redirectUrl: 'http://nickwanders.com/projects/ng-spotify-importer/'
+  }
 };
