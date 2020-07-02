a06c185149775dc222bcd947ceb9c9e05aea1c18
Nick <nick@networkbasedsolutions.com>
2020-07-02 14:16:41 -0700

    Added Angular's material library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
-import { SpotifyAuthComponent } from './spotify-auth/spotify-auth.component';
-import { PlaylistEditorComponent } from './playlist-editor/playlist-editor.component';
-import { FileReaderComponent } from './file-reader/file-reader.component';
+import {SpotifyAuthComponent} from './spotify-auth/spotify-auth.component';
+import {PlaylistEditorComponent} from './playlist-editor/playlist-editor.component';
+import {FileReaderComponent} from './file-reader/file-reader.component';
 import {FormsModule} from '@angular/forms';
-import { NotificationComponent } from './notification/notification.component';
+import {NotificationComponent} from './notification/notification.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NotificationComponent } from './notification/notification.component';
   imports: [
     BrowserModule,
     RouterModule.forRoot([]),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
