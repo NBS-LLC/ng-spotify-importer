30642c18e4e987acd4ce2f3153222271adf4947f
Nick <nick@networkbasedsolutions.com>
2020-08-20 16:47:23 -0700

    Fixed the default end to end tests.
    * Fixed the expected page title / header.
    * Fixed the locator used to find the page title / header.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-slacker-to-spotify app is running!');
+    expect(page.getTitleText()).toEqual('NG Spotify Importer');
   });
 
   afterEach(async () => {
