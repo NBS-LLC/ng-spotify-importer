0e4927166c7cd10dd1416cf14331ee71a4c0e557
Nick <nick@networkbasedsolutions.com>
2020-08-21 15:37:19 -0700

    Added unit tests for the Spotify service's generate methods.
    * Configured Jasmine / Karma to use a headless Chrome browser.

--- src/app/spotify.service.spec.ts
+++ src/app/spotify.service.spec.ts
@@ -18,4 +18,19 @@ describe('SpotifyService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should generate a code verifier', () => {
+    const code = service.generateCodeVerifier();
+    expect(code.length).toEqual(56);
+    expect(code).toMatch(/[0-9a-fA-F]+/);
+  });
+
+  it('should generate a URL safe code challenge', (done) => {
+    const code = 'd7988a4d0d5c5d6be3a8d4424c0f227e9a2f154f5b712cc0443df02a';
+    service.generateCodeChallenge(code).then(challenge => {
+      // Base64 would normally generate: 1TzPHyyCW0Uc23/t2qSQUdQvXdsL+gmacEQm8zLTfvo= which isn't URL safe.
+      expect(challenge).toEqual('1TzPHyyCW0Uc23_t2qSQUdQvXdsL-gmacEQm8zLTfvo');
+      done();
+    });
+  });
 });
