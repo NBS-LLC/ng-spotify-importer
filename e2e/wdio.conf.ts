684d47bb8ffaf1589809a86d1d9744861fc26de9
NBS-LLC <nick@networkbasedsolutions.com>
2022-01-10 15:39:26 -0800

    Allow Dynamic E2E Debugging
    * Added a new e2e debug script to package.json.
    * Added debug handling to WDIO's configuration.

--- e2e/wdio.conf.ts
+++ e2e/wdio.conf.ts
@@ -1,5 +1,13 @@
 import { dir } from "console";
 import { dirname } from "path";
+import logger from "@wdio/logger";
+
+const DEBUG = process.env['DEBUG'];
+
+const log = logger('wdio.conf.ts');
+if (DEBUG) {
+    log.info('Debugging is enabled.');
+}
 
 export const config: WebdriverIO.Config = {
     //
@@ -46,7 +54,7 @@ export const config: WebdriverIO.Config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: DEBUG ? 1 : 10,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -57,7 +65,7 @@ export const config: WebdriverIO.Config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        // maxInstances: 5,
         //
         browserName: 'chrome',
         acceptInsecureCerts: true,
@@ -145,7 +153,7 @@ export const config: WebdriverIO.Config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: DEBUG ? (24 * 60 * 60 * 1000) : 60000
     },
     //
     // =====
