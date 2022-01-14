e1f257d156ffc398463227d498c8f60a50ec9b76
NBS-LLC <nick@networkbasedsolutions.com>
2022-01-13 22:44:53 -0800

    Implementing the Simple Slacker Playlist e2e test.
    * Added an XML DOM lib as a dev dependency to the project.
    * Updated the e2e test to retrieve the expected song count from the test playlist.

--- e2e/test/specs/import-playlist.ts
+++ e2e/test/specs/import-playlist.ts
@@ -1,8 +1,10 @@
+import { DOMParser } from "@xmldom/xmldom";
 import { dirname } from "path";
 import fileReaderComponent from "../pages/file-reader-component";
 import playlistEditorComponent from "../pages/playlist-editor-component";
 import spotifyAuthComponent from "../pages/spotify-auth-component";
 import config from "../support/config";
+import { fileToString } from "../support/helpers";
 
 describe('import playlist flows', () => {
     it('simple slacker playlist', async () => {
@@ -13,13 +15,20 @@ describe('import playlist flows', () => {
         );
 
         const playlistPath = dirname(__filename) + '/../assets/playlists/80sHitsPlaylist.xml';
+        const expectedSongCount = getSongCountFromSpotifyPlaylist(playlistPath);
+
         await fileReaderComponent.waitForDisplayed();
         await fileReaderComponent.uploadSlackerPlaylist(playlistPath);
         await fileReaderComponent.waitForPlaylistToLoad();
 
         await playlistEditorComponent.waitForDisplayed();
         const allSongsLabel = await playlistEditorComponent.allSongsLabelElement.getText();
-        expect(allSongsLabel).toEqual('All Songs (60):');
+        expect(allSongsLabel).toEqual(`All Songs (${expectedSongCount}):`);
     });
 });
 
+function getSongCountFromSpotifyPlaylist(playlistPath: string): number {
+    const contents = fileToString(playlistPath);
+    const doc = new DOMParser().parseFromString(contents, 'text/xml');
+    return doc.getElementsByTagName('song').length;
+}
