7e7cb0d67d372c4c87ef67ccf706243266ae871c
NBS-LLC <nick@networkbasedsolutions.com>
2022-01-16 14:16:55 -0800

    Implementing the Simple Slacker Playlist e2e test.
    * Created a notification component page object.
    * Added selectors for the playlist editor's import button, and playlist name.
    * Added an action to import a playlist.
    * Re-added the logger for verbose test details.
    * Added importing and verify as part of the e2e test.
    * Added an id to the playlist editor's import button.

--- e2e/test/pages/playlist-editor-component.ts
+++ e2e/test/pages/playlist-editor-component.ts
@@ -3,12 +3,28 @@ class PlaylistEditorComponent {
         return $('<app-playlist-editor />');
     }
 
+    get playlistImportElement() {
+        return $('#playlist-import');
+    }
+
     get allSongsLabelElement() {
         return $("//label[@for='song-data-filter-all']");
     }
 
     get knownSongsLabelElement() {
         return $("//label[@for='song-data-filter-known']");
+    }
+
+    get playlistNameElement() {
+        return $('#playlist-name');
+    }
+
+    async importPlaylist(name: string = null) {
+        if (name) {
+            await this.playlistNameElement.setValue(name);
+        }
+
+        await this.playlistImportElement.click();
     }
 
     async waitForDisplayed() {
--- e2e/test/specs/import-playlist.ts
+++ e2e/test/specs/import-playlist.ts
@@ -1,12 +1,16 @@
+import logger from "@wdio/logger";
 import { dirname } from "path";
 import fileReaderComponent from "../pages/file-reader-component";
+import notificationComponent from "../pages/notification-component";
 import playlistEditorComponent from "../pages/playlist-editor-component";
 import spotifyAuthComponent from "../pages/spotify-auth-component";
 import config from "../support/config";
 import { getSongCountFromSpotifyPlaylist, parseSongCountFromLabel } from "../support/helpers";
 
+const log = logger('e2e - import playlist flows');
+
 describe('import playlist flows', () => {
-    describe('simple slacker playlist', () => {
+    describe('simple slacker playlist', async () => {
         const playlistPath = dirname(__filename) + '/../assets/playlists/80sHitsPlaylist.xml';
         const expectedSongCount = getSongCountFromSpotifyPlaylist(playlistPath);
 
@@ -32,6 +36,20 @@ describe('import playlist flows', () => {
             const knownSongsLabel = await playlistEditorComponent.knownSongsLabelElement.getText();
             const knownSongCount = parseSongCountFromLabel(knownSongsLabel);
             expect(knownSongCount).toBeGreaterThanOrEqual(Math.floor(expectedSongCount / 2));
+        });
+
+        it('should be able to import the playlist', async () => {
+            const uid = Date.now();
+            const playlistName = `NGSI QA Auto - ${uid}`;
+            await playlistEditorComponent.importPlaylist(playlistName);
+            await notificationComponent.waitForDisplayed();
+            const notificationMessage = await notificationComponent.componentElement.getText();
+
+            const knownSongsLabel = await playlistEditorComponent.knownSongsLabelElement.getText();
+            const knownSongCount = parseSongCountFromLabel(knownSongsLabel);
+            expect(notificationMessage).toEqual(`SUCCESS: Playlist imported with ${knownSongCount} tracks.`);
+
+            log.info(`Imported playlist: ${playlistName}`);
         });
     });
 });
