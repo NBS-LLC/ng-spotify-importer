312c219b02f8f7d88ccd707653da67418686b44f
NBS-LLC <nick@networkbasedsolutions.com>
2022-01-26 18:00:35 -0800

    WIP: New E2E Test - Fix Unknown Song
    * Added the log messages as a skeleton.

---
+++ e2e/test/specs/modify-playlist.ts
@@ -0,0 +1,12 @@
+suite('modify playlist flows', function () {
+    test('fix unknown song', async function () {
+        console.log('Load a playlist that contains an unknown song.');
+        console.log('Display more (related) songs.');
+        console.log('Modify the title and artist.');
+        console.log('Search for more (related) songs.');
+        console.log('Select the correct (related) known song.');
+        console.log('Import the modified playlist into Spotify.');
+        console.log('Verify the Spotify playlist contains all of the known songs.');
+        console.log('Verify the Spotify playlist contains the fixed (previously unknown) song.');
+    });
+});
