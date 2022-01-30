7b508302ae3fbdede0f5e9408e682aff47f59eec
NBS-LLC <nick@networkbasedsolutions.com>
2022-01-29 17:05:11 -0800

    New E2E Test - Fix Unknown Song
    * Finished implementation of e2e test.

--- e2e/test/specs/modify-playlist.ts
+++ e2e/test/specs/modify-playlist.ts
@@ -1,10 +1,12 @@
 import { dirname } from 'path';
 import fileReaderComponent from '../pages/file-reader-component';
+import notificationComponent from '../pages/notification-component';
 import playlistEditorComponent from '../pages/playlist-editor-component';
 import { songDetailsComponent } from '../pages/song-details-component';
 import spotifyAuthComponent from '../pages/spotify-auth-component';
+import { SpotifyClient } from '../services/spotify-client';
 import config from '../support/config';
-import { getSongCountFromCSVPlaylist, parseSongCountFromLabel } from '../support/helpers';
+import { getSongCountFromCSVPlaylist, parsePlaylistIdFromImportNotification, parseSongCountFromLabel } from '../support/helpers';
 
 suite('modify playlist flows', function () {
     test('fix unknown song', async function () {
@@ -44,14 +46,37 @@ suite('modify playlist flows', function () {
 
         console.log('Select the correct (related) known song.');
 
-        await songDetailsComponent.clickSearchResultRowByTitle('Twilight vs Breathe (feat. HALIENE & Matthew Steeper) - Jack Trades Remix');
+        const modifiedSongTitle = 'Twilight vs Breathe (feat. HALIENE & Matthew Steeper) - Jack Trades Remix';
+        await songDetailsComponent.clickSearchResultRowByTitle(modifiedSongTitle);
 
         const expectedKnownSongCount = getSongCountFromCSVPlaylist(playlistPath);
         const actualKnownSongCount = parseSongCountFromLabel(await playlistEditorComponent.knownSongsLabelElement.getText());
         expect(actualKnownSongCount).toEqual(expectedKnownSongCount);
 
         console.log('Import the modified playlist into Spotify.');
+
+        const uid = Date.now();
+        const playlistName = `NGSI QA Auto - ${uid}`;
+        await playlistEditorComponent.importPlaylist(playlistName);
+
+        await notificationComponent.waitForDisplayed();
+        const notificationMessage = await notificationComponent.componentElement.getText();
+        expect(notificationMessage).toContain('SUCCESS');
+        expect(notificationMessage).toContain(`${actualKnownSongCount} tracks`);
+
         console.log('Verify the Spotify playlist contains all of the known songs.');
+
+        const spotifyClient = await SpotifyClient.getInstance();
+        const playlistId = parsePlaylistIdFromImportNotification(notificationMessage);
+        const playlistDetails = await spotifyClient.getPlaylistDetailsById(playlistId);
+        expect(playlistDetails.body.name).toEqual(playlistName);
+        expect(playlistDetails.body.tracks.total).toEqual(actualKnownSongCount);
+
         console.log('Verify the Spotify playlist contains the fixed (previously unknown) song.');
+
+        const found = playlistDetails.body.tracks.items.find((item) => item.track.name === modifiedSongTitle);
+        expect(found).toBeTruthy();
+
+        console.log(`Imported playlist name: ${playlistName}.`);
     });
 });
