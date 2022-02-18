7bf9498f04ddc777e2a9d8957b94aa435ea3161d
NBS-LLC <nick@networkbasedsolutions.com>
2022-02-17 23:10:07 -0800

    WIP: Enhancing the Deployment Script
    * Renamed the deploy:upload script to: upload (its not really a child or variant of the deploy script)
    * Finished the implementation of the upload script.

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,3 +1,4 @@
+import { dirname } from 'path';
 import * as Client from 'ssh2-sftp-client';
 
 const sftp = new Client('deployment-uploader');
@@ -11,9 +12,21 @@ const sftp = new Client('deployment-uploader');
     });
 
     const remoteHomePath = process.env.NG_SPOTIFY_IMPORTER__DEPLOYMENT_HOME_PATH;
-    const files = await sftp.list(remoteHomePath);
+    const remoteProjectPath = remoteHomePath + '/' + process.env.NG_SPOTIFY_IMPORTER__DEPLOYMENT_PROJECT_PATH;
+
+    try {
+        await sftp.rmdir(remoteProjectPath, true);
+    } catch {
+        // Nothing to cleanup, so do nothing.
+    }
+
+    await sftp.mkdir(remoteProjectPath, true);
+
+    const htaccessFilePath = dirname(__filename) + '/../dist/.htaccess';
+    await sftp.fastPut(htaccessFilePath, remoteProjectPath + '/.htaccess');
+
+    const localProjectPath = dirname(__filename) + '/../dist/ng-slacker-to-spotify/';
+    await sftp.uploadDir(localProjectPath, remoteProjectPath);
 
     await sftp.end();
-
-    console.log(files);
 })();
