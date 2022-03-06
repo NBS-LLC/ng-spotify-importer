c719d64d11d72db1fb103077c305b9c433e20de4
NBS-LLC <nick@networkbasedsolutions.com>
2022-03-06 15:16:46 -0800

    WIP: New Feature - Cleanup Unknown Songs

--- src/lib/song-utilities.spec.ts
+++ src/lib/song-utilities.spec.ts
@@ -5,15 +5,15 @@ import { cleanupSong } from './song-utilities';
 
 describe('song-utilities', () => {
     describe('cleanupSong()', () => {
-        const expectedSong: Song = {
-            artist: 'Unit Test',
-            title: 'Sample Song Data'
-        };
-
         it('should remove the parenthesis part of the song title', () => {
             const song: Song = {
                 artist: 'Unit Test',
                 title: '(This Should be Removed) Sample(This Too) Song Data (And This)'
+            };
+
+            const expectedSong: Song = {
+                artist: 'Unit Test',
+                title: 'Sample Song Data'
             };
 
             const cleanSong = cleanupSong(song);
@@ -22,6 +22,11 @@ describe('song-utilities', () => {
 
         it('should not remove anything if parenthesis do not exist', () => {
             const song: Song = {
+                artist: 'Unit Test',
+                title: 'Sample Song Data'
+            };
+
+            const expectedSong: Song = {
                 artist: 'Unit Test',
                 title: 'Sample Song Data'
             };
@@ -36,6 +41,11 @@ describe('song-utilities', () => {
                 title: 'Sample Song Data & This Should be Removed'
             };
 
+            const expectedSong: Song = {
+                artist: 'Unit Test',
+                title: 'Sample Song Data'
+            };
+
             const cleanSong = cleanupSong(song);
             assert.deepStrictEqual(cleanSong, expectedSong);
         });
@@ -46,17 +56,55 @@ describe('song-utilities', () => {
                 title: '\tSample  Song Data   '
             };
 
+            const expectedSong: Song = {
+                artist: 'Unit Test',
+                title: 'Sample Song Data'
+            };
+
             const cleanSong = cleanupSong(song);
             assert.deepStrictEqual(cleanSong, expectedSong);
         });
 
-        it('should not the ampersand portion if its at the beginning of the song title', () => {
+        it('should not remove the ampersand portion if its at the beginning of the song title', () => {
             const song: Song = {
                 artist: 'Unit Test',
                 title: '& (This Should &be Removed) Sample Song Data'
             };
 
-            expectedSong.title = '& Sample Song Data';
+            const expectedSong: Song = {
+                artist: 'Unit Test',
+                title: '& Sample Song Data'
+            };
+
+            const cleanSong = cleanupSong(song);
+            assert.deepStrictEqual(cleanSong, expectedSong);
+        });
+
+        it('should remove the ampersand portion from the artist name', () => {
+            const song: Song = {
+                artist: 'Unit Test & This Should be Removed',
+                title: 'Sample Song Data'
+            };
+
+            const expectedSong: Song = {
+                artist: 'Unit Test',
+                title: 'Sample Song Data'
+            };
+
+            const cleanSong = cleanupSong(song);
+            assert.deepStrictEqual(cleanSong, expectedSong);
+        });
+
+        it('should not remove the ampersand portion if its the start of the artist name', () => {
+            const song: Song = {
+                artist: '& Super Unit Test',
+                title: 'Sample Song Data'
+            };
+
+            const expectedSong: Song = {
+                artist: '& Super Unit Test',
+                title: 'Sample Song Data'
+            };
 
             const cleanSong = cleanupSong(song);
             assert.deepStrictEqual(cleanSong, expectedSong);
