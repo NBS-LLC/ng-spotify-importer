4b84fc050e66a3fe1d0b1784ce4d690c9d5ed103
NBS-LLC <nick@networkbasedsolutions.com>
2022-03-23 23:05:29 -0700

    Reverted unrelated change.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,40 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import SpotifyWebApi from 'spotify-web-api-js';
-import { AppComponent } from './app.component';
-import { FileReaderComponent } from './file-reader/file-reader.component';
-import { NotificationComponent } from './notification/notification.component';
-import { PlaylistEditorComponent } from './playlist-editor/playlist-editor.component';
-import { SongDetailsComponent } from './song-details/song-details.component';
-import { SpotifyAuthComponent } from './spotify-auth/spotify-auth.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {SpotifyAuthComponent} from './spotify-auth/spotify-auth.component';
+import {PlaylistEditorComponent} from './playlist-editor/playlist-editor.component';
+import {FileReaderComponent} from './file-reader/file-reader.component';
+import {FormsModule} from '@angular/forms';
+import {NotificationComponent} from './notification/notification.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SongDetailsComponent} from './song-details/song-details.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {HttpClientModule} from '@angular/common/http';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }),
-    FormsModule,
-    BrowserAnimationsModule,
-    MatDialogModule,
-    HttpClientModule,
-  ],
-  declarations: [
-    AppComponent,
-    SpotifyAuthComponent,
-    PlaylistEditorComponent,
-    FileReaderComponent,
-    NotificationComponent,
-    SongDetailsComponent
-  ],
-  providers: [
-    { provide: 'SpotifyWebApiJs', useClass: SpotifyWebApi }
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }),
+        FormsModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        HttpClientModule,
+    ],
+    declarations: [
+        AppComponent,
+        SpotifyAuthComponent,
+        PlaylistEditorComponent,
+        FileReaderComponent,
+        NotificationComponent,
+        SongDetailsComponent
+    ],
+    providers: [
+        { provide: 'SpotifyWebApiJs', useClass: SpotifyWebApi }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
