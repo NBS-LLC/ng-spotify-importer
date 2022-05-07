edd813c80b4647e61f12b8e3badffb84776300b5
NBS-LLC <nick@networkbasedsolutions.com>
2022-05-07 14:16:07 -0700

    Removed unused auth code.

--- e2e/test/pages/spotify-auth-component.ts
+++ e2e/test/pages/spotify-auth-component.ts
@@ -1,7 +1,5 @@
-import { Credentials } from '../support/credentials';
 import { setSpotifyAuthToken } from '../support/helpers';
 import authorizePage from './vendor/spotify/authorize-page';
-import spotifyLoginPage from './vendor/spotify/login-page';
 
 class SpotifyAuthComponent {
     get componentElement() {
@@ -15,18 +13,6 @@ class SpotifyAuthComponent {
     async grantPermissionWithCookies() {
         await this.grantElement.click();
         await setSpotifyAuthToken();
-        await authorizePage.waitForDisplayed();
-        await authorizePage.clickAgree();
-    }
-
-    async grantPermissionWithCredentials(credentials: Credentials) {
-        await this.grantElement.click();
-        try {
-            await spotifyLoginPage.waitForDisplayed();
-            await spotifyLoginPage.loginWithCredentials(credentials);
-        } catch {
-            console.warn('Spotify login page not displayed. Already logged in?');
-        }
         await authorizePage.waitForDisplayed();
         await authorizePage.clickAgree();
     }
