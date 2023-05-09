f66276046a63d969e8787c5fb3e87c978935ff7f
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-05-08 17:40:46 -0700

    Fix E2E Test Data Cleanup
    * Updated the E2E code to use named instances of the TestDataManager.

--- e2e/test/support/test-data-manager.ts
+++ e2e/test/support/test-data-manager.ts
@@ -3,6 +3,13 @@ export class TestDataManager {
     private static instances: TestDataManager[] = [];
     private playlistNames: string[] = [];
 
+    /**
+     * Get a named instance of this class.
+     * 
+     * Note: it is important that a named instance be used when running specs
+     * in parallel. Each process should have its own instance to prevent
+     * conflicts. If a name is not provided a default will be used.
+     */
     public static getInstance(name: string = 'default') {
         return this.instances[name] || (this.instances[name] = new this());
     }
