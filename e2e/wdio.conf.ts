d040365b06fc23e7a57aa751fb8d3de739d7003f
nderevj <nick@networkbasedsolutions.com>
2025-08-16 17:49:05 -0700

    chrome 139 blocks public to private redirects

--- e2e/wdio.conf.ts
+++ e2e/wdio.conf.ts
@@ -73,7 +73,7 @@ export const config: WebdriverIO.Config = {
         // maxInstances: 5,
         //
         browserName: 'chrome',
-        browserVersion: 'stable',
+        browserVersion: '138',
         acceptInsecureCerts: true,
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
