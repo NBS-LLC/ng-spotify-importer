a57896583ece61559ea7acb826060f42209fb03a
Nick <nick@networkbasedsolutions.com>
2025-10-15 16:23:12 -0700

    chore: use https redirects (#294)

--- e2e/test/deploy-specs/deployment-configuration.ts
+++ e2e/test/deploy-specs/deployment-configuration.ts
@@ -6,7 +6,7 @@ suite('deployment configuration tests', function () {
       await browser.url('https://stg.nickwanders.com/projects/ng-spotify-importer/');
       const grantUrl = await $('[data-testid="grant-link"]').getAttribute('href');
       expect(grantUrl).toContain('client_id=75be9215050a42a1907c7dcf53b0e199');
-      expect(grantUrl).toContain('redirect_uri=http://stg.nickwanders.com/projects/ng-spotify-importer/');
+      expect(grantUrl).toContain('redirect_uri=https://stg.nickwanders.com/projects/ng-spotify-importer/');
     });
 
     test('release link', async function () {
--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   spotify: {
     clientId: '75be9215050a42a1907c7dcf53b0e199',
-    redirectUrl: 'http://stg.nickwanders.com/projects/ng-spotify-importer/',
+    redirectUrl: 'https://stg.nickwanders.com/projects/ng-spotify-importer/',
   },
 };
