bdd5a463bf7edaa422bd3f6d204645306b058428
nderevj <nick@networkbasedsolutions.com>
2025-10-15 16:13:55 -0700

    spotify now requires https redirects

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   spotify: {
     clientId: '2c25063414a6495db2f265babde58010',
-    redirectUrl: 'http://dev.nickwanders.com/projects/ng-spotify-importer/',
+    redirectUrl: 'https://dev.nickwanders.com/projects/ng-spotify-importer/',
   },
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   spotify: {
     clientId: '710c3609ed2b4605b3a17464001b1398',
-    redirectUrl: 'http://nickwanders.com/projects/ng-spotify-importer/',
+    redirectUrl: 'https://nickwanders.com/projects/ng-spotify-importer/',
   },
 };
